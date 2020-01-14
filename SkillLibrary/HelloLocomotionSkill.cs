2c91648c3f9325e4edc7b8f10871a9c56f59261c
akeecha <59363456+akeecha@users.noreply.github.com>
2020-01-14 20:45:51 +1100

    Add facial recognition and voice recognitio

--- SkillLibrary/HelloLocomotionSkill.cs
+++ SkillLibrary/HelloLocomotionSkill.cs
@@ -1,22 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
-using MistyRobotics.SDK.Messengers;
 using MistyRobotics.SDK.Events;
 using MistyRobotics.Common.Types;
 using MistyRobotics.SDK;
 using MistyRobotics.Common.Data;
+using MistyRobotics.SDK.Messengers;
 
 namespace SkillLibrary
 {
 	/// <summary>
-	/// Simple fun skill that just drives based upon time of flight data
-	/// Place your hand in front of the time of flights to make it drive in the opposite direction
-	/// Closer to the tofs makes it move faster
+	/// Skill to demonstrate simple driving and the hazards
 	/// Also adds most of the previous skills' functionality to give the robot a fuller personality
 	/// Assumes defaults assets are on the robot, update the names of assets if they have changed
 	/// </summary>
-	public class ForceDriving : IMistySkill
+	public class HelloLocomotionSkill : IMistySkill
 	{
 		/// <summary>
 		/// Robot reference
@@ -34,6 +32,11 @@ namespace SkillLibrary
 		private Random _randomGenerator = new Random();
 
 		/// <summary>
+		/// Timer object to perform callbacks at a interval to process the data and do the next action
+		/// </summary>
+		private Timer _heartbeatTimer;
+
+		/// <summary>
 		/// Timer object to perform callbacks at a regular interval to move the head
 		/// </summary>
 		private Timer _moveHeadTimer;
@@ -49,6 +52,11 @@ namespace SkillLibrary
 		private Timer _ledTimer;
 
 		/// <summary>
+		/// Flag to indicate the direction the robot is moving
+		/// </summary>
+		private bool _goingForward = false;
+
+		/// <summary>
 		/// List of images on the robot
 		/// </summary>
 		private IList<ImageDetails> _imageList { get; set; } = new List<ImageDetails>();
@@ -57,6 +65,7 @@ namespace SkillLibrary
 		/// List of audio files on the robot
 		/// </summary>
 		private IList<AudioDetails> _audioList { get; set; } = new List<AudioDetails>();
+
 
 		#region Required Skill Methods, Event Handlers & Accessors
 
@@ -72,9 +81,9 @@ namespace SkillLibrary
 		///   BroadcastMode - different modes can be set to share different levels of information from the robot using the 'SkillData' websocket
 		///   AllowedCleanupTimeInMs - How long to wait after calling OnCancel before denying messages from the skill and performing final cleanup  
 		/// </summary>
-		public INativeRobotSkill Skill { get; private set; } = new NativeRobotSkill("ForceDriving", "e886e306-0ac1-49d8-9933-7d469546633a")
+		public INativeRobotSkill Skill { get; private set; } = new NativeRobotSkill("HelloLocomotionSkill", "600ea0a9-e5f7-433d-88f5-492f72e343ca")
 		{
-			TimeoutInSeconds = 60 * 10 //runs for 10 minutes or until the skill is cancelled
+			TimeoutInSeconds = 60 * 5  //runs for 5 minutes or until the skill is cancelled
 		};
 
 		/// <summary>
@@ -85,10 +94,8 @@ namespace SkillLibrary
 		public void LoadRobotConnection(IRobotMessenger robotInterface)
 		{
 			_misty = robotInterface;
-			_misty.SkillLogger.LogLevel = SkillLogLevel.Verbose;
-
 		}
-
+		
 		/// <summary>
 		/// Get the assets and startup some timers to do random movements and other things...
 		/// </summary>
@@ -100,24 +107,25 @@ namespace SkillLibrary
 				//Get the audio and image lists for use in the skill
 				_audioList = (await _misty.GetAudioListAsync())?.Data;
 				_imageList = (await _misty.GetImageListAsync())?.Data;
-
+				
 				_misty.Wait(2000);
-
-				_misty.PlayAudio("s_Awe.wav", 100, null);
-
+				
+				_misty.PlayAudio("s_Awe.wav", 100, null); 
 				_misty.ChangeLED(255, 255, 255, null);
 				_misty.DisplayImage("e_ContentRight.jpg", 1, null);
 
+				_misty.StartKeyPhraseRecognition(null);
+				_misty.StartFaceRecognition(null);
 				RegisterEvents();
 
-				//_heartbeatTimer = new Timer(HeartbeatCallback, null, 3000, 3000);
-				_moveHeadTimer = new Timer(MoveHeadCallback, null, 10000, 10000);
-				_moveArmsTimer = new Timer(MoveArmCallback, null, 10000, 10000);
+				_heartbeatTimer = new Timer(HeartbeatCallback, null, 3000, 3000);
+				_moveHeadTimer = new Timer(MoveHeadCallback, null, 5000, 10000);
+				_moveArmsTimer = new Timer(MoveArmCallback, null, 5000, 4000);
 				_ledTimer = new Timer(ChangeLEDCallback, null, 2000, 2000);
 			}
 			catch (Exception ex)
 			{
-				_misty.SkillLogger.Log($"ForceDriving : OnStart: => Exception", ex);
+				_misty.SkillLogger.Log($"HelloLocomotionSkill : OnStart: => Exception", ex);
 			}
 		}
 
@@ -127,8 +135,7 @@ namespace SkillLibrary
 		/// <param name="parameters"></param>
 		public void OnCancel(object sender, IDictionary<string, object> parameters)
 		{
-
-			_misty.SkillLogger.LogVerbose($"ForceDriving : OnCancel called");
+			_misty.SkillLogger.LogVerbose($"HelloLocomotionSkill : OnCancel called");
 			DoCleanup();
 			_misty.ChangeLED(255, 0, 0, null);
 		}
@@ -138,7 +145,7 @@ namespace SkillLibrary
 		/// </summary>
 		public void OnTimeout(object sender, IDictionary<string, object> parameters)
 		{
-			_misty.SkillLogger.LogVerbose($"ForceDriving : OnTimeout called");
+			_misty.SkillLogger.LogVerbose($"HelloLocomotionSkill : OnTimeout called");
 			DoCleanup();
 			_misty.ChangeLED(0, 0, 255, null);
 		}
@@ -150,7 +157,7 @@ namespace SkillLibrary
 		public void OnPause(object sender, IDictionary<string, object> parameters)
 		{
 			//In this example, Pause is not implemented by default and that command is ignored
-			_misty.SkillLogger.LogVerbose($"ForceDriving : OnPause called");
+			_misty.SkillLogger.LogVerbose($"HelloLocomotionSkill : OnPause called");
 		}
 
 		/// <summary>
@@ -160,7 +167,7 @@ namespace SkillLibrary
 		public void OnResume(object sender, IDictionary<string, object> parameters)
 		{
 			//In this example, Resume is not implemented by default and that command is ignored
-			_misty.SkillLogger.LogVerbose($"ForceDriving : OnResume called");
+			_misty.SkillLogger.LogVerbose($"HelloLocomotionSkill : OnResume called");
 		}
 
 		/// <summary>
@@ -186,6 +193,7 @@ namespace SkillLibrary
 			if (disposing)
 			{
 				// Free any other managed objects here.
+				_heartbeatTimer?.Dispose();
 				_moveArmsTimer?.Dispose();
 				_moveHeadTimer?.Dispose();
 				_ledTimer?.Dispose();
@@ -198,90 +206,135 @@ namespace SkillLibrary
 		/// <summary>
 		/// Skill Finalizer/Destructor
 		/// </summary>
-		~ForceDriving()
+		~HelloLocomotionSkill()
 		{
 			Dispose(false);
+		}
+		
+		#endregion
+
+		
+		#region User Created Helper Methods
+
+		/// <summary>
+		/// Do timeout or cancel cleanup
+		/// </summary>
+		private void DoCleanup()
+		{
+			_misty.Stop(null);
+			_misty.StopKeyPhraseRecognition(null);
+			_misty.StopFaceRecognition(null);
 		}
 
 		#endregion
 
 		#region User Created Helper Methods
 
-		private void DoCleanup()
-		{
-			// Stop Driving
-			_misty.Stop(null);
-			// Stop KeyPhrase recognition
-			//_misty.StopKeyPhraseRecognition(null);
-			// Stop Face Recognition
-			_misty.StopFaceRecognition(null);
-		}
-
-		#endregion
-
-		#region  User Created Callbacks
-
-		private void MoveHeadCallback(object info)
-		{
-			_misty.MoveHead(_randomGenerator.Next(-30, 15), _randomGenerator.Next(-30, 30), _randomGenerator.Next(-70, 70), _randomGenerator.Next(10, 30), AngularUnit.Degrees, null);
-		}
-
-		private void MoveArmCallback(object info)
-		{
-			_misty.MoveArms(_randomGenerator.Next(-90, 90), _randomGenerator.Next(-90, 90), 100, 100, null, AngularUnit.Degrees, null);
-		}
-
-		private void ChangeLEDCallback(object info)
-		{
-			_misty.ChangeLED((uint)_randomGenerator.Next(0, 256), (uint)_randomGenerator.Next(0, 256), (uint)_randomGenerator.Next(0, 256), null);
-		}
-
 		/// <summary>
 		/// Register the desired startup events, more may be registered separately as needed
 		/// </summary>
 		private void RegisterEvents()
 		{
+			//Voice Rec
+			_misty.RegisterKeyPhraseRecognizedEvent(KeyPhraseRecognizedCallback, 0, true, null, null);
+
+			//Face rec
+			_misty.RegisterFaceRecognitionEvent(FaceRecCallback, 0, false, null, null, null);
+
 			//Bumps
 			_misty.RegisterBumpSensorEvent(BumpCallback, 0, true, null, null, null);
-
+			
 			//Cap touch
 			_misty.RegisterCapTouchEvent(CapTouchCallback, 0, true, null, null, null);
-
-			// Face Recognition
-			_misty.RegisterFaceRecognitionEvent(FaceRecCallback, 0, true, null, null, null);
-			_misty.StartFaceRecognition(null);
-
-			// Key Phase
-			//_misty.RegisterKeyPhraseRecognizedEvent(KeyPhraseRecognizedCallback, 250, true, null, null);
-			//_misty.StartKeyPhraseRecognition(null);
-
+			
 			//TOF
 			List<TimeOfFlightValidation> tofFrontRightValidations = new List<TimeOfFlightValidation>();
-			tofFrontRightValidations.Add(new TimeOfFlightValidation { Name = TimeOfFlightFilter.DistanceInMeters, Comparison = ComparisonOperator.LessThanOrEqual, ComparisonValue = 0.3 });
+			tofFrontRightValidations.Add(new TimeOfFlightValidation { Name = TimeOfFlightFilter.DistanceInMeters, Comparison = ComparisonOperator.LessThanOrEqual, ComparisonValue = 0.15 });
 			tofFrontRightValidations.Add(new TimeOfFlightValidation { Name = TimeOfFlightFilter.SensorName, Comparison = ComparisonOperator.Equal, ComparisonValue = TimeOfFlightPosition.FrontRight });
-			_misty.RegisterTimeOfFlightEvent(TOFRangeCallback, 300, true, tofFrontRightValidations, null, null);
+			_misty.RegisterTimeOfFlightEvent(TOFRangeCallback, 50, true, tofFrontRightValidations, null, null);
 
 			List<TimeOfFlightValidation> tofFrontLeftValidations = new List<TimeOfFlightValidation>();
-			tofFrontLeftValidations.Add(new TimeOfFlightValidation { Name = TimeOfFlightFilter.DistanceInMeters, Comparison = ComparisonOperator.LessThanOrEqual, ComparisonValue = 0.3 });
+			tofFrontLeftValidations.Add(new TimeOfFlightValidation { Name = TimeOfFlightFilter.DistanceInMeters, Comparison = ComparisonOperator.LessThanOrEqual, ComparisonValue = 0.15 });
 			tofFrontLeftValidations.Add(new TimeOfFlightValidation { Name = TimeOfFlightFilter.SensorName, Comparison = ComparisonOperator.Equal, ComparisonValue = TimeOfFlightPosition.FrontLeft });
-			_misty.RegisterTimeOfFlightEvent(TOFRangeCallback, 300, true, tofFrontLeftValidations, null, null);
+			_misty.RegisterTimeOfFlightEvent(TOFRangeCallback, 50, true, tofFrontLeftValidations, null, null);
 
 			List<TimeOfFlightValidation> tofBackValidations = new List<TimeOfFlightValidation>();
-			tofBackValidations.Add(new TimeOfFlightValidation { Name = TimeOfFlightFilter.DistanceInMeters, Comparison = ComparisonOperator.LessThanOrEqual, ComparisonValue = 0.3 });
+			tofBackValidations.Add(new TimeOfFlightValidation { Name = TimeOfFlightFilter.DistanceInMeters, Comparison = ComparisonOperator.LessThanOrEqual, ComparisonValue = 0.15 });
 			tofBackValidations.Add(new TimeOfFlightValidation { Name = TimeOfFlightFilter.SensorName, Comparison = ComparisonOperator.Equal, ComparisonValue = TimeOfFlightPosition.Back });
-			_misty.RegisterTimeOfFlightEvent(TOFRangeCallback, 300, true, tofBackValidations, null, null);
-
-			List<TimeOfFlightValidation> tofFrontCenterValidations = new List<TimeOfFlightValidation>();
-			tofFrontCenterValidations.Add(new TimeOfFlightValidation { Name = TimeOfFlightFilter.DistanceInMeters, Comparison = ComparisonOperator.LessThanOrEqual, ComparisonValue = 0.3 });
-			tofFrontCenterValidations.Add(new TimeOfFlightValidation { Name = TimeOfFlightFilter.SensorName, Comparison = ComparisonOperator.Equal, ComparisonValue = TimeOfFlightPosition.FrontCenter });
-			_misty.RegisterTimeOfFlightEvent(TOFRangeCallback, 300, true, tofFrontCenterValidations, null, null);
-
+			_misty.RegisterTimeOfFlightEvent(TOFRangeCallback, 50, true, tofBackValidations, null, null);
 
 		}
 
 		#endregion
 
-		#region User Created Callback Delegates
+		#region User Created Callbacks
+		
+		/// <summary>
+		/// Called on the heartbeat timer tick to assess the current state of the robot and make a driving decision
+		/// </summary>
+		/// <param name="info"></param>
+		public void HeartbeatCallback(object info)
+		{
+			_goingForward = !_goingForward;
+
+			if (!_misty.Wait(0)) { return; }
+
+			if (_randomGenerator.Next(1, 5) == 1)
+			{
+				var angular = _randomGenerator.Next(10, 15);
+				if (_randomGenerator.Next(1, 2) == 1)
+				{
+					angular = -angular;
+				}
+				_misty.Drive(0, angular, null);
+				_misty.SkillLogger.Log($"HelloLocomotionSkill : DRIVING: linear 0 angular {angular}");
+			}
+			else
+			{
+				var linear = _randomGenerator.Next(15, 25);
+
+				var angular = _randomGenerator.Next(0, linear - 15 < 0 ? 0 : linear - 15);
+				if (_randomGenerator.Next(1, 2) == 1)
+				{
+					angular = -angular;
+				}
+
+				if (!_goingForward)
+				{
+					linear = -linear;
+				}
+
+				_misty.Drive(linear, angular, null);
+				_misty.SkillLogger.Log($"HelloLocomotionSkill : DRIVING: linear {linear} angular {angular}");
+			}
+		}
+
+		/// <summary>
+		/// Called on the timer tick to send a random move head command to the robot
+		/// </summary>
+		/// <param name="info"></param>
+		public void MoveHeadCallback(object info)
+		{
+			_misty.MoveHead(_randomGenerator.Next(-30, 15), _randomGenerator.Next(-30, 30), _randomGenerator.Next(-70, 70), _randomGenerator.Next(10, 30), AngularUnit.Degrees, null);
+		}
+
+		/// <summary>
+		/// Called on the timer tick to send a random move arm command to the robot
+		/// </summary>
+		/// <param name="info"></param>
+		public void MoveArmCallback(object info)
+		{
+			_misty.MoveArms(_randomGenerator.Next(-90, 90), _randomGenerator.Next(-90, 90), _randomGenerator.Next(10, 40), _randomGenerator.Next(10, 40), null, AngularUnit.Degrees, null);
+		}
+
+		/// <summary>
+		/// Called on the timer tick to send a random change LED command to the robot
+		/// </summary>
+		/// <param name="info"></param>
+		public void ChangeLEDCallback(object info)
+		{
+			_misty.ChangeLED((uint)_randomGenerator.Next(0, 256), (uint)_randomGenerator.Next(0, 256), (uint)_randomGenerator.Next(0, 256), null);
+		}
 
 		/// <summary>
 		/// Callback called when the bump sensor is contacted or released
@@ -289,16 +342,16 @@ namespace SkillLibrary
 		/// </summary>
 		/// <param name="bumpEvent"></param>
 		private void BumpCallback(IBumpSensorEvent bumpEvent)
-		{
-			if (!bumpEvent.IsContacted)
+		{			
+			if(!bumpEvent.IsContacted)
 			{
 				return;
 			}
 
-			_misty.SkillLogger.Log($"ForceDriving : BumpCallback: => {bumpEvent.IsContacted}");
+			_misty.SkillLogger.Log($"HelloLocomotionSkill : BumpCallback: => {bumpEvent.IsContacted}");
 			_misty.Stop(null);
 
-			switch (bumpEvent.SensorPosition)
+			switch(bumpEvent.SensorPosition)
 			{
 				case BumpSensorPosition.FrontRight:
 					_misty.PlayAudio("s_PhraseHello.wav", 100, null);
@@ -321,48 +374,26 @@ namespace SkillLibrary
 		/// <param name="tofEvent"></param>
 		private void TOFRangeCallback(ITimeOfFlightEvent tofEvent)
 		{
-			_misty.SkillLogger.Log($"{(tofEvent.Status != 0 ? "WARNING!! " : "")}ForceDriving : TOFRangeCallback {tofEvent.SensorPosition.ToString()} - Status:{tofEvent.Status} - Meters:{tofEvent.DistanceInMeters}");
+			_misty.SkillLogger.Log($"HelloLocomotionSkill : TOFRangeCallback {tofEvent.SensorPosition.ToString()} {tofEvent.DistanceInMeters} - goingforward = {_goingForward}");
 
-			switch (tofEvent.SensorPosition)
+			if (tofEvent.DistanceInMeters <= 0.1 && tofEvent.Status == 0)
 			{
-				case TimeOfFlightPosition.FrontLeft:
-				case TimeOfFlightPosition.FrontRight:
-				case TimeOfFlightPosition.FrontCenter:
-					if (tofEvent.Status == 0)
-					{
-						if (tofEvent.DistanceInMeters <= 0.05)
-						{
-							_misty.DriveTime(-25, 0, 1000, null);
-						}
-						else if (tofEvent.DistanceInMeters <= 0.1)
-						{
-							_misty.DriveTime(-15, 0, 1000, null);
-						}
-						else if (tofEvent.DistanceInMeters <= 0.15)
-						{
-							_misty.DriveTime(-10, 0, 1000, null);
-						}
-						_misty.MoveArms(-60, -60, 100, 100, null, AngularUnit.Degrees, null);
-					}
-					break;
-				case TimeOfFlightPosition.Back:
-					if (tofEvent.Status == 0)
-					{
-						if (tofEvent.DistanceInMeters <= 0.05)
-						{
-							_misty.DriveTime(25, 0, 1000, null);
-						}
-						if (tofEvent.DistanceInMeters <= 0.1)
-						{
-							_misty.DriveTime(15, 0, 1000, null);
-						}
-						if (tofEvent.DistanceInMeters <= 0.15)
-						{
-							_misty.DriveTime(10, 0, 1000, null);
-						}
-						_misty.MoveArms(-30, -30, 100, 100, null, AngularUnit.Degrees, null);
-					}
-					break;
+				if (_goingForward &&
+					(tofEvent.SensorPosition == TimeOfFlightPosition.FrontLeft ||
+					tofEvent.SensorPosition == TimeOfFlightPosition.FrontRight ||
+					tofEvent.SensorPosition == TimeOfFlightPosition.FrontCenter))
+				{
+			
+					_misty.SkillLogger.Log($"HelloLocomotionSkill : Driving forward attempt while something in front");
+					_misty.Stop(null);
+
+				}
+				else if (!_goingForward && tofEvent.SensorPosition == TimeOfFlightPosition.Back)
+				{
+				
+					_misty.SkillLogger.Log($"HelloLocomotionSkill : Driving backward attempt while something behind");
+					_misty.Stop(null);
+				}
 			}
 		}
 
@@ -373,7 +404,7 @@ namespace SkillLibrary
 		/// <param name="capTouchEvent"></param>
 		private void CapTouchCallback(ICapTouchEvent capTouchEvent)
 		{
-			if (capTouchEvent.IsContacted)
+			if(capTouchEvent.IsContacted)
 			{
 				switch (capTouchEvent.SensorPosition)
 				{
@@ -386,8 +417,8 @@ namespace SkillLibrary
 						_misty.DisplayImage("e_Amazement.jpg", 1, null);
 						break;
 					case CapTouchPosition.Right:
-						_misty.PlayAudio("s_Joy.wav", 100, null);
-						_misty.DisplayImage("e_JoyGoofy3.jpg", 1, null);
+						_misty.PlayAudio("Joy.wav", 100, null);
+						_misty.DisplayImage("JoyGoofy.jpg", 1, null);
 						break;
 					case CapTouchPosition.Left:
 						_misty.PlayAudio("e_Terror.jpg", 100, null);
@@ -411,13 +442,11 @@ namespace SkillLibrary
 		/// <param name="voiceEvent"></param>
 		private void KeyPhraseRecognizedCallback(IKeyPhraseRecognizedEvent voiceEvent)
 		{
-			_misty.SkillLogger.Log($"ForceDriving : KeyPhraseRecognizedCallback: => {voiceEvent.EventId}");
-			_misty.DisplayImage("e_Disoriented.jpg", 1, null);
-			_misty.PlayAudio("s_DisorientedConfused.wav", 100, null);
+			_misty.DisplayImage("e_Love.jpg", 1, null);
+			_misty.PlayAudio("s_Awe.wav", 100, null);
 
-			if (!_misty.Wait(3000)) { return; }
+			if(!_misty.Wait(3000)) { return; }
 			_misty.StartKeyPhraseRecognition(null);
-
 		}
 
 		/// <summary>
@@ -435,25 +464,12 @@ namespace SkillLibrary
 			}
 			else
 			{
-				_misty.SkillLogger.Log($"ForceDriving : FaceRecCallback: => {faceRecEvent.Distance}");
-
-				if (faceRecEvent.Label == "Adrian")
-				{
-					_misty.DisplayImage("e_EcstacyStarryEyed.jpg", 1, null);
-					_misty.PlayAudio("s_Ecstacy.wav", 100, null);
-				}
-				else 
-				{
-					if (faceRecEvent.Label == "Sharina")
-					{
-						_misty.DisplayImage("e_Love.jpg", 1, null);
-						_misty.PlayAudio("s_Love.wav", 80, null);
-					}
-				}
+				_misty.DisplayImage("e_EcstacyStarryEyed.jpg", 1, null);
+				_misty.PlayAudio("sEcstacy.wav", 100, null);
 			}
-				
-			if (!_misty.Wait(5000)) { return; }
-			_misty.DisplayImage("e_DefaultContent.jpg", 1, null);
+			
+			if(!_misty.Wait(5000)) { return; }
+			_misty.DisplayImage("e_ContentLeft.jpg", 1, null);
 			if (!_misty.Wait(5000)) { return; }
 			_misty.RegisterFaceRecognitionEvent(FaceRecCallback, 0, false, null, null, null);
 		}
